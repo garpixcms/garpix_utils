b1846974a3c04c095f12beb0bf406f22bad579c2
crusat <crusat@yandex.ru>
2021-07-21 11:04:35 +0300

    Убран objects.

--- backend/garpix_utils/models/active_mixin.py
+++ backend/garpix_utils/models/active_mixin.py
@@ -4,7 +4,6 @@ from ..managers.active_manager import ActiveManager
 
 class ActiveMixin(models.Model):
     is_active = models.BooleanField(default=True, verbose_name='Включено')
-    objects = models.Manager()
     active_objects = ActiveManager()
 
     class Meta:
