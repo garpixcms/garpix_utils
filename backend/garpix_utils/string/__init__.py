28df79f0b5df5f395dea97edbc1712cf647b1f68
Alexandr Kilchevskiy <alexkill94@mail.ru>
2024-03-19 16:02:39 +0300

    hash fix

--- backend/garpix_utils/string/__init__.py
+++ backend/garpix_utils/string/__init__.py
@@ -1,5 +1,6 @@
 import random
 import string
+from uuid import uuid4
 
 
 def get_random_string(size=8, chars=string.ascii_uppercase + string.digits):
@@ -8,6 +9,10 @@ def get_random_string(size=8, chars=string.ascii_uppercase + string.digits):
     """
     return ''.join(random.SystemRandom().choice(chars) for _ in range(size))
 
+def get_uuid4_hash(size=32):
+    uuid = uuid4()
+    return str(uuid).replace('-', '')[:size]
+
 
 class GenerateHash:
 
@@ -15,7 +20,9 @@ class GenerateHash:
         self.hash_length = hash_length
 
     def __call__(self):
-        return get_random_string(self.hash_length, chars=string.ascii_letters + string.digits)
+        if self.hash_length > 32:
+            return get_random_string(self.hash_length, chars=string.ascii_letters + string.digits)
+        return get_uuid4_hash(self.hash_length)
 
     def deconstruct(self):
         return ('garpix_utils.string.GenerateHash', [self.hash_length], {})
