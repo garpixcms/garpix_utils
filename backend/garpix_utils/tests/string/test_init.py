c98237b40f88547677d8a33e77bc5d597c5ddafa
Jacob Gulyaev <guljaev_ja@garpix.com>
2024-12-09 10:28:53 +0300

    GARPIXCMS-213 Added tests for get_file_path, UploadTo, get_random_string and get_uuid4_hash.

---
+++ backend/garpix_utils/tests/string/test_init.py
@@ -0,0 +1,42 @@
+import string
+from unittest.mock import patch
+
+from rest_framework.test import APITestCase
+
+from garpix_utils.string import get_random_string, get_uuid4_hash
+
+
+class GetRandomStringTest(APITestCase):
+    def setUp(self) -> None:
+        self.params = [
+            [
+                (8, string.ascii_uppercase),
+                (string.digits, string.ascii_lowercase),
+            ],
+            [
+                (16, string.ascii_lowercase),
+                (string.digits, string.ascii_uppercase),
+            ],
+            [
+                (32, string.digits),
+                (string.ascii_lowercase, string.ascii_uppercase)
+            ]
+        ]
+
+    def test_get_random_string(self) -> None:
+        for param in self.params:
+            rand = get_random_string(*param[0])
+            self.assertEqual(len(rand), param[0][0])
+            self.assertFalse(set(param[1][0] + param[1][1]).intersection(set(rand)))
+
+
+class GetUuid4HashTest(APITestCase):
+    @patch("garpix_utils.string.uuid4")
+    def test_get_uuid4_hash(self, uuid4) -> None:
+        uuid4.return_value = "UU-ID-4"
+        self.assertEqual(get_uuid4_hash(), "UUID4")
+
+
+class GenerateHashTest(APITestCase):
+    def setUp(self) -> None:
+        ...
