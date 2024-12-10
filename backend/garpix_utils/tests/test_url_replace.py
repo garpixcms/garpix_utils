7419da52a4c30df6a63bf0f1f299989d94de418a
Jacob Gulyaev <guljaev_ja@garpix.com>
2024-12-10 12:04:52 +0300

    GARPIXCMS-213 Added test for url_replace.

--- backend/garpix_utils/tests/test_url_replace.py
+++ backend/garpix_utils/tests/test_url_replace.py
@@ -15,3 +15,8 @@ class UrlReplaceTest(APITestCase):
         self.assertEqual(request.GET["test2"], "test_value2")
 
         self.assertEqual(query_string, "test1=test_1&test2=test_value2")
+
+    def test_url_replace_not_exists(self) -> None:
+        request = HttpRequest()
+        query_string = url_replace(request, "test", "value")
+        self.assertEqual(query_string, "test=value")
